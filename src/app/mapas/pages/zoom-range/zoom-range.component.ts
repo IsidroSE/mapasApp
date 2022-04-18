5c3ad2eddbe0472119fe2b80c4c3cae46c1d5c71
Isidro Sotoca <Isidre10_92@hotmail.es>
2022-04-18 16:14:26 +0200

    add zoom management

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styles: [
-  ]
+  styles: [`
+  div.mapa-container {
+    width: 100%;
+    height: 100%;
+  }
+
+  div.row {
+    background-color: white;
+    bottom: 50px;
+    left: 50px;
+    padding: 10px;
+    border-radius: 10px;
+    position: fixed;
+    z-index: 999;
+  }
+`]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-0.277761, 39.069170],
+      zoom: 16
+    });
+
   }
+
+  zoomOut() {
+    this.mapa.zoomOut();
+  }
+
+  zoomIn() {
+    this.mapa.zoomIn();
+  }
+
 
 }
